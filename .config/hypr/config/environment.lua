834e7c1e762b11b34480a2ab05ab9e6ad50f7260
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-06-18 17:17:00 -0400

    created and set background to red

---
+++ .config/hypr/config/environment.lua
@@ -0,0 +1,47 @@
+hl.config({
+    general = {
+        gaps_in = 5,
+        gaps_out = 20,
+
+        border_size = 2,
+
+        col = {
+            active_border = { colors = {"rgba(ff3333ee)", "rgba(8b0000ee)"}, angle = 45 },
+            inactive_border = "rgba(222222aa)",
+        },
+
+        -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
+        resize_on_border = false,
+
+        -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+        allow_tearing = false,
+        layout = "dwindle",
+    },
+
+    decoration = {
+        rounding = 10,
+        rounding_power = 2,
+
+        -- Change transparency of focused and unfocused windows
+        active_opacity = 1.0,
+        inactive_opacity = 1.0,
+
+        shadow = {
+            enabled = true,
+            range = 4,
+            render_power = 3,
+            color = 0xee1a1a1a,
+        },
+
+        blur = {
+            enabled = true,
+            size = 3,
+            passes = 1,
+            vibrancy = 0.1696,
+        },
+    },
+
+    animations = {
+        enabled = true,
+    },
+})
