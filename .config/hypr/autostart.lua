b0520bd8838d7b1de646beeaad32ebc8266ee94b
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-06-17 23:19:53 -0400

    move and reformat autostart

--- .config/hypr/autostart.lua
+++ .config/hypr/autostart.lua
@@ -7,7 +7,12 @@
 
 hl.on("hyprland.start", function () 
     -- Start polkit daemon
-    hl.exec("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+    hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+
+    -- Environment for xdg-desktop-portal-hyprland
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+
+    -- Start hyprpaper for wallpaper management
+    hl.exec_cmd("hyprpaper")
 
 end)
